7508990e71cdaa187f0088c9103186ef7ed406bd
senayaraya-56 <senay.araya56@gmail.com>
2026-03-17 15:52:19 -0400

    Add files via upload

---
+++ drugDiffusion1D.m
@@ -0,0 +1,28 @@
+function [C] = drugDiffusion1D(D, L, T, Nx, Nt, decay)
+
+dx = L/(Nx-1);
+dt = T/Nt;
+
+if dt > dx^2/(2*D)
+    error('Time step too large!');
+end
+
+C = zeros(Nx, Nt);
+
+% Boundary condition (Change and add to as appropriate for the question at hand)
+C(1,:) = 1;
+
+for t_idx = 1:Nt-1
+    for i = 2:Nx-1
+        C(i,t_idx+1) = C(i,t_idx) + D*dt/dx^2 * ...
+            (C(i+1,t_idx) - 2*C(i,t_idx) + C(i-1,t_idx)) ...
+            - decay*C(i,t_idx)*dt;
+    end
+
+    C(1,t_idx+1) = 1;
+    C(Nx,t_idx+1) = C(Nx-1,t_idx+1);
+end
+
+end
+
+% Use this in the command window. [C] = drugDiffusion1D(Put the inputs you have)
